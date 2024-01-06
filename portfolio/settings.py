6236b7a5161d5afad0e9a0fd7733dc2bb401572b
Azizbek G'ulomov <azizbekgulomovweb@gmail>
2024-01-06 14:39:11 +0500

    f

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-b5hk@clyiybqbj+5g1o5#jva27g1mga8b7u^7_rr8jhd&)c+i#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh", 'localhost']
 
